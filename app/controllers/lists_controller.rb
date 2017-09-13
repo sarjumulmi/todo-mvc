a634eb2ed2042a8fb494afeec9649d83fb72ac65
sarjumulmi <mulmi.sarju@gmail.com>
2017-09-12 21:31:07 -0400

    Create and retrieve Lists

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,22 @@
+class ListsController < ApplicationController
+
+  def index
+    @lists = List.all
+    @list = List.new
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def create
+    @list = List.create(list_params)
+    redirect_to lists_path
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end
